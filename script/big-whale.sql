abbeeaa6255faac270ad26889d7b91659eeba019
suxiaoyang <653800209@qq.com>
2021-08-17 08:33:05 +0800

    数据脚本重复字段修复

--- script/big-whale.sql
+++ script/big-whale.sql
@@ -459,8 +459,7 @@ ALTER TABLE `script_history`
 	CHANGE COLUMN `state` `state` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci' AFTER `content`,
 	CHANGE COLUMN `steps` `steps` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci' AFTER `state`,
 	ADD COLUMN `job_params` VARCHAR(255) NULL DEFAULT NULL AFTER `finish_time`,
-	DROP COLUMN `schedule_snapshot_id`,
-	ADD COLUMN `submit_time` DATETIME NULL DEFAULT NULL AFTER `create_by`;
+	DROP COLUMN `schedule_snapshot_id`;
 
 
 DROP TABLE `schedule_snapshot`;
